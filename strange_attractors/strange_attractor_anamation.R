d639ef16aa21be27c6d604ad24af80ed8557f1aa
Isaac Faber <isaacfab@gmail.com>
2019-12-19 11:16:59 -0500

    updating some data and image

--- strange_attractors/strange_attractor_anamation.R
+++ strange_attractors/strange_attractor_anamation.R
@@ -27,6 +27,8 @@ clifford_data <- data.frame(x = rep(0,n),
                             y = rep(0,n))
 
 # with a large n this will take quite a while
+# I already did this for 1 million points and it is saved here as clifford_1.RData
+
 pb <- progress_bar$new(total = n)
 for(i in 1:n){
   temp <- clifford_attractor(a,b,c,d,x_n,y_n)
@@ -37,7 +39,28 @@ for(i in 1:n){
   pb$tick()
 }
 
-ggplot(clifford_data[1:1000,], aes(x = x, y = y)) +
-  geom_path(linejoin = "round",alpha=0.1) +
+ggplot(clifford_data[1:100,], aes(x = x, y = y)) +
+  geom_point() +
   coord_equal() +
-  theme_void()
+  theme_void() -> plot
+
+ggsave("clifford_1.png", plot, height = 5, width = 5, units = 'in')
+
+ani.options(interval = 0.05, 
+            nmax = 300,
+            ani.width = 480,
+            ani.height = 480)
+
+saveVideo({
+  num_iters <- 10
+  for(i in 1:num_iters){
+    clifford_data_subset <- clifford_data[1:(100000*i),]
+    #write the plot with a subset
+    p<-ggplot(clifford_data_subset, aes(x = x, y = y)) +
+      geom_point(shape=46,alpha=0.01) +
+      coord_equal() +
+      theme_void() 
+    print(p)
+  }#close the for loop
+  
+}, video.name = "clifford_1.mp4", other.opts = "-pix_fmt yuv420p -b 300k")
