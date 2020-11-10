000e2357ccde9aa28057448e288d18cd41af3473
Isaac Faber <isaacfab@gmail.com>
2020-11-10 08:51:50 -0500

    updating some directions

--- learn_r/Start Here.R
+++ learn_r/Start Here.R
@@ -1,12 +1,17 @@
 ##########################################################################################
-# If you haven't done it yet Forklift this project and create an RStudio tool
+# If you haven't done it yet install RStudio: https://rstudio.com/products/rstudio/download/
 #
-# Welcome to R and RStudio!                                                            ^
+# After installation run RStudio!                                                            ^
 # This script is just a starter for the awesome swirl package                         ^^^  
-# Step 1: Higlight line 12 of this file and then press run -------------------up here ^^^^^
+# Step 1: Higlight lines 12-17 of this file and then press run -------------------up here ^^^^^
 # Step 2: Flow the directions!
 # Step 3: Learn R
 # Step 4: Go to the Tidyverse script and work through Hadley Wickhams book!
 ##########################################################################################
 
-library(swirl)
+if(!require(swirl)){
+    install.packages("swirl")
+    library(swirl)
+} else {
+    library(swirl)
+}
